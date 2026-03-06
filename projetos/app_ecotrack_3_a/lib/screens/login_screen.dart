191d9b80ef51b0e8a2414c2205bfce24700b403b
ebarrado <erika_barrado@hotmail.com>
2026-03-06 14:57:46 -0300

    Finalização Tela de Login com Gradiente sob imagem

--- projetos/app_ecotrack_3_a/lib/screens/login_screen.dart
+++ projetos/app_ecotrack_3_a/lib/screens/login_screen.dart
@@ -22,15 +22,30 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,        
           children: [
-            Center(
-              child: Text("EcoTrack",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.green[400]
+            Stack(
+              //alignment: Alignment.bottomLeft,
+              children:[              
+                Image.asset(
+                "logo_ecotrack.png",
+                //width: 150,
+                height: 150,
+                fit: BoxFit.cover,
               ),
-              ),
-            ),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end:Alignment.topCenter,
+                    colors: [
+                     const Color.fromARGB(0, 243, 243, 243),
+                      const Color.fromARGB(100, 15, 83, 38),
+                      
+                    ]),
+                ),
+              )]),
+            SizedBox(height: 150,),          
             TextField(
               controller: emailController,
               decoration: InputDecoration(
@@ -38,6 +53,60 @@ class _LoginScreenState extends State<LoginScreen> {
                 prefixIcon: Icon(Icons.email),
                 border: OutlineInputBorder()
               ),
+            ),
+            SizedBox(height: 50,),
+            TextField(
+              controller: senhaController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white
+                ), 
+                child: Text(
+                  "Criar Conta",
+                  style: TextStyle(
+                    fontFamily: "Arial",
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  ),
+              ),
+
+            SizedBox(height: 50 ,),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(                              
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                
+                ),
+                onPressed: (){
+                  print("Relizar Login");
+                }, 
+                child: Text(
+                  "Entrar",
+                  style: TextStyle(
+                    fontFamily: "Arial",
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  ),
+                  
+                  )
+                ),
             )
           ],
         ),
