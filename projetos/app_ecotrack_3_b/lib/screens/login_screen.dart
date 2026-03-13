2a3cc4b223e87ca0fb741bd662b028b891a34175
ebarrado <erika_barrado@hotmail.com>
2026-03-13 13:19:02 -0300

    Projeto Tela Login TextFormField e auth_service

--- projetos/app_ecotrack_3_b/lib/screens/login_screen.dart
+++ projetos/app_ecotrack_3_b/lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formkey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +50,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               SizedBox(height: 150),
               TextFormField(
+                controller: emailController,
                 decoration: InputDecoration(
                   labelText: 'E-mail',
                   prefix: Icon(Icons.email),
@@ -63,6 +66,80 @@ class _LoginScreenState extends State<LoginScreen> {
                     borderSide: const BorderSide(
                       color: Color.fromARGB(255, 5, 110, 14),
                       width: 3,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return "Digite seu E-mail";
+                  }
+                  if(!value.contains("@") || value.contains(".")){
+                    return "E-mail Inválido";
+                  }
+                  return null;
+                } ,
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefix: Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 5, 110, 14),
+                      width: 6,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                      color: Color.fromARGB(255, 5, 110, 14),
+                      width: 3,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(                  
+                  style: ButtonStyle(                    
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.green,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Criar Conta",
+                    style: TextStyle(
+                      fontFamily: "Arial",
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.green,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(
+                      fontFamily: "Arial",
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                 ),
